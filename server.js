0f5422139c44f11a8c1dda8616d37251324278ff
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-02-07 16:02:33 -0500

    feat: setup socket.io with users broadcasting

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when a clietn connects
+io.on('connection', (socket) => {
+	// console.log('new ws connectiion');
+
+	// welcome current user
+	socket.emit('message', 'Welcome to Chat Buddy');
+
+	// notify to the current user that another user just connects
+	socket.broadcast.emit('message', 'A user just joined the chat');
+
+	// this runs when client disconnects
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left the chat :(');
+	});
+});
+
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`server running on ${PORT}`);
 });
